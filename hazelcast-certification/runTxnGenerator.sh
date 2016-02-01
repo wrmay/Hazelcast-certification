cadab12f4c3a6c22b2aeebb63fb080d4f521006c
Rahul Gupta <rahul@hazelcast.com>
2016-02-02 09:37:09 +1300

    Added scripts

--- hazelcast-certification/runTxnGenerator.sh
+++ hazelcast-certification/runTxnGenerator.sh
@@ -1,1 +1,4 @@
-java -Xms3g -Xmx4g -XX:+UseParallelOldGC -XX:+UseParallelGC -XX:+UseCompressedOops -classpath "/home/rahul/nfs/app/certification/lib/*":/home/rahul/nfs/app/certification/resources com.hazelcast.certification.util.TransactionsGeneratorRunner
+# modify the classpath according to your directory structure
+# "../nfs/app/certification/lib/*" contains hazelcast-xxx.jar, hazelcast-client-xxx.jar, joda-time-2.7.jar and the certification project jar - build with your own implementation
+# ../nfs/app/certification/resources contains FraudDetection.properties and hazelcast-client.xml
+java -Xms3g -Xmx4g -XX:+UseParallelOldGC -XX:+UseParallelGC -XX:+UseCompressedOops -classpath "../nfs/app/certification/lib/*":../nfs/app/certification/resources com.hazelcast.certification.util.TransactionsGeneratorRunner
