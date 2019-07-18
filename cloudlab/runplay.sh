e35d0966b94f532743764a7115cd3b0c28dc188e
Randy May <wrandymay@gmail.com>
2019-07-18 12:53:07 -0400

    update level of parallelism in Ansible

--- cloudlab/runplay.sh
+++ cloudlab/runplay.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 envname=$1
 play=$2
-ansible-playbook --inventory=$envname/inventory.ini --key=$envname/$envname.pem -e envname=$envname $play.yaml 
+ansible-playbook --inventory=$envname/inventory.ini --key=$envname/$envname.pem --forks=32 -e envname=$envname $play.yaml 
